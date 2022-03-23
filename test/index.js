2c9f4cc4f01c2a478156f3d28622af4a58965c57
jojoypeng <jojoypeng@tencent.com>
2022-03-23 16:02:31 +0800

    feat: use vitest

---
+++ test/index.js
@@ -0,0 +1,47 @@
+import {transformAsync} from '@babel/core'
+import {isBinaryExpression, logicalExpression, binaryExpression, isLogicalExpression} from '@babel/types'
+
+const binaryVisitor = {
+  BinaryExpression(path) {
+    const node = path.node
+    if (isBinaryExpression(node.left)) {
+      const right = binaryExpression(node.operator, node.left.right, node.right)
+      path.replaceWith(logicalExpression('&&',node.left, right));
+    }
+  }
+}
+
+const logicalVisitor = {
+  LogicalExpression(path) {
+    const node = path.node
+    if(isLogicalExpression(path.node.left) && !isBinaryExpression(path.node.right)) {
+      const { left, operator } = findFirstNode(node)
+      const right = binaryExpression(operator, left, node.right)
+      path.replaceWith(logicalExpression('||', node.left, right));
+    }
+    if (isBinaryExpression(node.left) && !isBinaryExpression(node.right)) {
+      const right = binaryExpression(node.left.operator, node.left.left, node.right)
+      path.replaceWith(logicalExpression('||', node.left, right));
+    }
+  }
+}
+
+function findFirstNode (node) {
+  if (isBinaryExpression(node.left)) {
+    return {
+      left: node.left.left,
+      operator: node.left.operator
+    }
+  } else {
+    return findFirstNode(node.left)
+  }
+}
+
+export async function thenTansform (code) {
+  return (await transformAsync(code, {
+    plugins:[
+      {visitor: logicalVisitor},
+      {visitor: binaryVisitor},
+    ]
+  })).code
+}

f7b08ee2029fbc4af1dfca1d4e4a2ad29ad0cb15
flippedround <734243792@qq.com>
2022-05-15 00:00:40 +0800

    chore:  添加eslint配置

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,24 @@
 import type { Plugin } from 'vite'
-import { createFilter, FilterPattern } from '@rollup/pluginutils'
-import {transformAsync} from '@babel/core'
-import {isBinaryExpression, logicalExpression, binaryExpression, isLogicalExpression} from '@babel/types'
+import type { FilterPattern } from '@rollup/pluginutils'
+import { createFilter } from '@rollup/pluginutils'
+import { transformAsync } from '@babel/core'
+import { binaryExpression, isBinaryExpression, isLogicalExpression, logicalExpression } from '@babel/types'
 
 const binaryVisitor = {
-  BinaryExpression(path:any) {
+  BinaryExpression(path: any) {
     const node = path.node
-    const operatorList = ['<','>','<=','>=']
-    if (isBinaryExpression(node.left) && operatorList.indexOf(node.operator) !== -1) {
+    const operatorList = ['<', '>', '<=', '>=']
+    if (isBinaryExpression(node.left) && operatorList.includes(node.operator)) {
       const right = binaryExpression(node.operator, node.left.right, node.right)
-      path.replaceWith(logicalExpression('&&',node.left, right))
+      path.replaceWith(logicalExpression('&&', node.left, right))
     }
-  }
+  },
 }
 
 const logicalVisitor = {
-  LogicalExpression(path:any) {
+  LogicalExpression(path: any) {
     const node = path.node
-    if(isLogicalExpression(node.left) && !isBinaryExpression(node.right)) {
+    if (isLogicalExpression(node.left) && !isBinaryExpression(node.right)) {
       const { left, operator } = findFirstNode(node)
       const right = binaryExpression(operator, left, node.right)
       path.replaceWith(logicalExpression(node.operator, node.left, right))
@@ -26,26 +27,27 @@ const logicalVisitor = {
       const right = binaryExpression(node.left.operator, node.left.left, node.right)
       path.replaceWith(logicalExpression(node.operator, node.left, right))
     }
-  }
+  },
 }
 
-function findFirstNode (node: any): any {
+function findFirstNode(node: any): any {
   if (isBinaryExpression(node.left)) {
     return {
       left: node.left.left,
-      operator: node.left.operator
+      operator: node.left.operator,
     }
-  } else {
+  }
+  else {
     return findFirstNode(node.left)
   }
 }
 
-async function thenTansform (code: string) {
+async function thenTansform(code: string) {
   return (await transformAsync(code, {
-    plugins:[
-      {visitor: logicalVisitor},
-      {visitor: binaryVisitor}
-    ]
+    plugins: [
+      { visitor: logicalVisitor },
+      { visitor: binaryVisitor },
+    ],
   }))?.code as string
 }
 
@@ -59,15 +61,14 @@ function VitePluginCompare(options: Options = {}): Plugin {
     name: 'vit-plugin-compare',
     enforce: 'post',
     async transform(code: string, id: string) {
-      const { 
+      const {
         include = /\.(js|tsx|vue|ts|tsx)/,
-        exclude = /node_modules/
+        exclude = /node_modules/,
       } = options
       const filter = createFilter(include, exclude)
-      if (filter(id)) {
+      if (filter(id))
         return { code: await thenTansform(code) }
-      }
-    }
+    },
   }
 }
 
